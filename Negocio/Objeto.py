77ec790a1f12dacbd0f2adfa63cd852d0dd426c5
Itamar UFMG <69490985+RamatiSeraos@users.noreply.github.com>
2026-05-31 20:42:40 -0300

    push push

--- Negocio/Objeto.py
+++ Negocio/Objeto.py
@@ -1,15 +1,15 @@
 from abc import ABC, abstractmethod
 
 class Objeto(ABC):
-    def __init__(self, id: int, titulo: str, descricao: str):
+    def __init__(self, id: str, titulo: str, descricao: str):
         self.validar_atributos(id, titulo, descricao)
         self.id = id
         self.titulo = titulo
         self.descricao = descricao
 
-    def validar_atributos(self, id: int, titulo: str, descricao: str):     
-        if not isinstance(id, str) or id.strip:
-            raise ValueError("O ID deve ser um inteiro positivo.")
+    def validar_atributos(self, id: str, titulo: str, descricao: str):     
+        if not isinstance(id, str) or not id.strip():
+            raise ValueError("O ID deve ser uma string não vazia.")
         
         if not isinstance(titulo, str) or not titulo.strip():
             raise ValueError("O título deve ser uma string não vazia.")
